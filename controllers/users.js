d65f992bf52f36e5f36525c98cd963cc302835de
David Terwilliger <terwilld@gmail.com>
2024-07-12 13:01:32 -0400

    add user show

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@
 const axios = require('axios')
 require('dotenv').config()
 const SNOWpw = process.env.SNOWpw;
-console.log(SNOWpw)
+// console.log(SNOWpw)
 
 function compare(a, b) {
     if (a.last_login > b.last_login_time) {
@@ -14,29 +14,29 @@ function compare(a, b) {
     return 0;
 }
 module.exports.users = async (req, res) => {
-
     const data = await axios.get(
-        //'https://{your instance}.service-now.com/api/now/v2/table/incident?sysparm_limit=1',
         'https://dev204219.service-now.com/api/now/table/sys_user',
         { headers: { "Accept": "application/json", "Content-Type": "application/json", "Authorization": ("Basic " + new Buffer(`admin:${SNOWpw}`).toString('base64')) } }
     )
     const users = data.data.result
     users.sort(compare)
-    for (user of users) {
-
-
-        if (user.user_name.includes("DELETE")) {
-            console.log(user.user_name)
-            console.log(user)
-        }
-    }
     res.render('users/indexusers.ejs', { users })
 }
 
+
 module.exports.showuser = async (req, res) => {
-    res.send("hello show user")
+    const uid = req.params.id
+
     console.log('I am in the show controller')
-    console.log(req.params.id)
+    console.log(uid)
+    const data = await axios.get(
+        `https://dev204219.service-now.com/api/now/table/sys_user/${uid}`,
+        { headers: { "Accept": "application/json", "Content-Type": "application/json", "Authorization": ("Basic " + new Buffer(`admin:${SNOWpw}`).toString('base64')) } }
+    )
+    // console.log(data)
+    user = data.data.result
+    console.log(user)
+    res.render('users/show.ejs', { user })
 }
 
 
