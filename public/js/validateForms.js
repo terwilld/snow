8d13c1eeba074bf076e2b5f8aba57752ec66646f
David Terwilliger <terwilld@gmail.com>
2024-07-15 18:57:19 -0400

    add form validation on new user

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -1,13 +1,15 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
-    bsCustomFileInput.init()
+    // bsCustomFileInput.init()
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.validated-form')
+    console.log("Is this actually running")
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
+            console.log("Test is this running")
             form.addEventListener('submit', function (event) {
                 if (!form.checkValidity()) {
                     event.preventDefault()
