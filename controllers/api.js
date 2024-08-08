a287b384a88a3112362982bc50bf4035094c7d2c
David Terwilliger <terwilld@gmail.com>
2024-08-08 11:21:59 -0400

    updated the post to update record

--- controllers/api.js
+++ controllers/api.js
@@ -12,6 +12,25 @@ module.exports.newAuditLogs = async (req, res) => {
 
 module.exports.updaterecordsnowid = async (req, res) => {
     console.log("Hit this end point")
-    res.send("LOL")
+    const auditLogs = require('../models/auditLogs.js')
+
+    console.log(req.body)
+    console.log(req.params)
+    const oid = req.params.id
+    console.log(oid)
+
+
+    try {
+        auditlog = await auditLogs.findOne({ _id: oid })
+        console.log(auditlog)
+        auditlog.snowID = req.body.sysid
+        await auditlog.save()
+        console.log(auditlog)
+        res.send("record was updated")
+    } catch (e) {
+        console.log(e)
+        res.send("Record was not updated")
+    }
+
 }
 
